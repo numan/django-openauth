39a715571664557d231d99b01cebe93217ee598f
Numan Sachwani <numan856@gmail.com>
2011-10-14 10:44:25 -0700

    Unique constraint causes issues in mysql.

--- openauth/openid/models.py
+++ openauth/openid/models.py
@@ -18,13 +18,13 @@ class Nonce(models.Model):
     salt = models.CharField(max_length=40)
 
     objects = managers.NonceManager()
-    
+
     class Meta:
         ordering = ['-timestamp']
 
     def __unicode__(self):
         return self.get_hash()
-    
+
     def is_expired(self):
         return self.timestamp + self.EXPIRES < time.time()
 
@@ -41,8 +41,8 @@ class Association(models.Model):
     TYPES = {
         'HMAC-SHA1': cryptutil.hmacSha1,
         'HMAC-SHA256': cryptutil.hmacSha256,
-    }    
-    
+    }
+
     server_url = models.TextField(max_length=2048, db_index=True)
     handle = models.CharField(max_length=255, db_index=True)
     secret = models.TextField(max_length=255, blank=False)
@@ -51,32 +51,32 @@ class Association(models.Model):
     assoc_type = models.TextField(max_length=64, choices=tuple([(t, t) for t in TYPES.keys()]))
 
     objects = managers.AssociationManager()
-    
+
     class Meta:
         ordering = ['-issued']
-        
+
     def __unicode__(self):
         return u'%s exp:%smin' % (self.server_url, self.timeleft()/60)
-    
+
     def timeleft(self):
         return int(self.issued) + int(self.lifetime) - int(time.time())
-        
+
     def is_expired(self):
         return self.timeleft < 0
-    
+
     @property
     def secret_key(self):
         return base64.b64decode(self.secret)
-    
+
     @secret_key.setter
     def secret_key(self, secret):
         self.secret = base64.b64encode(secret)
-    
+
     def sign(self, message):
         hash_func = self.TYPES[self.assoc_type]
         return hash_func(self.secret_key, message)
-         
+
 class OpenIDProfile(models.Model):
     user = models.ForeignKey(User)
-    claimed_id = models.TextField(max_length=2048, unique=True)
+    claimed_id = models.TextField(max_length=2048)
     display_id = models.TextField(max_length=2048, blank=True)  #rename to identity
